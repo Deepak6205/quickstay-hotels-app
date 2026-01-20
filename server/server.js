5ceba70d3873d098216a89a10f59191e745c96a7
deepak kumar <deepak7292832956@gmail.com>
2026-01-20 10:18:11 +0530

    user controller check

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebHooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 const app = express();
@@ -11,6 +12,9 @@ app.use(cors()) // Enable Corss-origin Resource sharing
 // middleware 
 app.use(express.json());
 app.use(clerkMiddleware());
+
+//API to listen clerk webhook
+app.use("/api/clerk",clerkWebHooks)
 
 app.get('/',(req,res)=>res.send("API IS WORKING FINE"));
 
